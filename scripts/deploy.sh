4b98383411c058a90fc28a4e8fa3de6ef66557bb
maga_bash <caitov.magomed@yandex.ru>
2022-06-01 12:32:07 +0300

    Deploy 2022-06-01

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm -rf docs build 
-react-scripts build 
+npm run build 
 mv build docs 
 sed 's/.\/static/\/incubators\/static/g ' -i docs/index.html 
 git add . 
-git commit -am deploy 
+git commit -am "Deploy `date --iso`" 
 git push origin react
