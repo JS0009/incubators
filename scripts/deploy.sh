eb331ed9a1c1c0f22d7b11117263840525802085
maga_bash <caitov.magomed@yandex.ru>
2022-06-03 12:44:47 +0300

    Deploy 2022-06-03

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 rm -rf docs build 
 npm run build 
 mv build docs 
+cp docs/index.html docs/index.local.html
 sed 's/\/static/\/incubators\/static/g' -i docs/index.html 
 git add . 
 git commit -am "Deploy `date --iso`" 
